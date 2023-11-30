76d58d84101f5b241a24db0343934f614ad5ceae
fisker <lionkay@gmail.com>
2023-11-30 04:33:23 +0000

    Fix bad merge

--- src/prettier_serial.ts
+++ src/prettier_serial.ts
@@ -14,6 +14,7 @@ import prettierPostcss from "prettier/plugins/postcss";
 import prettierTypescript from "prettier/plugins/typescript";
 import prettierYaml from "prettier/plugins/yaml";
 import type { FormatOptions } from "./types.js";
+import { resolve } from "./utils.js";
 
 //TODO: Avoid loading plugins until they are actually needed
 
