27f7f380dc655058a7e5b6e939b2ecca7370e017
Fabio Spampinato <spampinabio@gmail.com>
2023-12-12 14:27:36 +0100

    Ensuring the CLI loads its own version dynamically, without hard-coding it in the source

--- src/constants.ts
+++ src/constants.ts
@@ -1,10 +1,11 @@
 import { createRequire } from "node:module";
 
 const require = createRequire(import.meta.url);
-const pkg = require("prettier/package.json");
+const prettierPackage = require("prettier/package.json");
+const cliPackage = require("@prettier/cli/package.json");
 
-const PRETTIER_VERSION = pkg.version;
-const CLI_VERSION = "0.1.2"; //TODO: Hard-coding this is error-prone
+const PRETTIER_VERSION = prettierPackage.version;
+const CLI_VERSION = cliPackage.version;
 const IS_BUN = !!process.versions["bun"];
 
 export { PRETTIER_VERSION, CLI_VERSION, IS_BUN };
