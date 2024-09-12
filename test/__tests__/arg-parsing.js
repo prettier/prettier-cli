418c99b9b2767cd3aff48235d4b7100c128a23b8
Fabio Spampinato <spampinabio@gmail.com>
2024-09-12 21:08:57 +0100

    Ported test suite: arg-parsing
    - Unknown options cause an error rather than a warning.
    - The styling of some errors changed (colored, padded, indented).
    - The algorithm used for suggesting flags when unknown ones are encountered changed.
    - Deprecated options like "--jsx-bracket-same-line" are no longer recognized.

---
+++ test/__tests__/arg-parsing.js
@@ -0,0 +1,118 @@
+import {runCli} from "../utils";
+
+describe("boolean flags do not swallow the next argument", () => {
+  runCli("arg-parsing", [
+    "--end-of-line",
+    "lf",
+    "--single-quote",
+    "file.js",
+  ]).test({
+    status: 0,
+  });
+});
+
+describe("negated options work", () => {
+  runCli("arg-parsing", [
+    "--end-of-line",
+    "lf",
+    "--no-semi",
+    "file.js",
+  ]).test({
+    status: 0,
+  });
+});
+
+describe("unknown options are errored", () => {
+  runCli("arg-parsing", [
+    "--end-of-line",
+    "lf",
+    "file.js",
+    "--unknown",
+  ]).test({
+    status: 1,
+  });
+});
+
+describe("unknown negated options are errored", () => {
+  runCli("arg-parsing", [
+    "--end-of-line",
+    "lf",
+    "file.js",
+    "--no-unknown",
+  ]).test({
+    status: 1,
+  });
+});
+
+describe("unknown options may trigger a suggestion `_`", () => {
+  runCli("arg-parsing", [
+    "file.js",
+    "-a",
+  ]).test({
+    status: 1,
+    write: [],
+  });
+});
+
+describe("allow overriding flags", () => {
+  runCli("arg-parsing", [
+    "--tab-width=1",
+    "--tab-width=3",
+    "--parser=babel",
+  ], {
+    input: "function a() { b }",
+  }).test({
+    stdout: "function a() {\n   b;\n}",
+    status: 0,
+  });
+});
+
+describe("number file/dir", () => {
+  const patterns = ["1", "2.2", "3", "4.44"];
+  for (const pattern of patterns) {
+    runCli("arg-parsing/number", [
+      "--parser=babel",
+      "--list-different",
+      pattern,
+    ]).test({
+      stderr: "",
+      status: 1,
+      write: [],
+    });
+  }
+  runCli("arg-parsing/number", [
+    "--parser=babel",
+    "--list-different",
+    ...patterns,
+  ]).test({
+    stderr: "",
+    status: 1,
+    write: [],
+  });
+});
+
+describe.skip("options with `cliName` should not allow to pass directly", () => {
+  // `filepath` can only pass through `--stdin-filepath`
+  // `plugins` works the same
+  runCli("arg-parsing", [
+    "--stdin-filepath",
+    "file.js",
+  ], {
+    isTTY: false,
+    input: "prettier()",
+  }).test({
+    status: 0,
+    stderr: "",
+    write: [],
+  });
+  runCli("arg-parsing", [
+    "--filepath",
+    "file.js",
+  ], {
+    isTTY: false,
+    input: "prettier()",
+  }).test({
+    status: 2,
+    write: [],
+  });
+});
---
+++ test/utils.js
@@ -0,0 +1,87 @@
+import exec from "nanoexec";
+import path from "node:path";
+import process from "node:process";
+
+import { expect, test } from "@jest/globals";
+import serializerRaw from "jest-snapshot-serializer-raw";
+import serializerAnsi from "jest-snapshot-serializer-ansi";
+
+expect.addSnapshotSerializer(serializerRaw);
+expect.addSnapshotSerializer(serializerAnsi);
+
+const ROOT_PATH = process.cwd();
+const BIN_PATH = path.join(ROOT_PATH, "dist", "bin.js");
+const FIXTURES_PATH = path.join(ROOT_PATH, "test", "__fixtures__");
+
+function normalizeOutput(output, options) {
+  // \r is trimmed from jest snapshots by default;
+  // manually replacing this character with /*CR*/ to test its true presence
+  // If ignoreLineEndings is specified, \r is simply deleted instead
+  // output = output.replace(/\r/gu, options.ignoreLineEndings ? "" : "/*CR*/"); //TODO
+  output = output.replace(/(\r?\n|\r)$/, "");
+  return output;
+}
+
+async function runCommand(dir, args, options) {
+  const cwd = path.join(FIXTURES_PATH, dir);
+  const result = exec("node", [BIN_PATH, ...args], { cwd, stdio: "pipe" });
+
+  if (options.input) {
+    result.process.stdin.write(options.input);
+    result.process.stdin.end();
+  }
+
+  const status = await result.code;
+  const stdout = normalizeOutput((await result.stdout).toString());
+  const stderr = normalizeOutput((await result.stderr).toString());
+  const write = []; //TODO
+
+  return { status, stdout, stderr, write };
+}
+
+async function runTest(name, expected, result, options) {
+  const title = options.title || "";
+  test(`${title}(${name})`, async () => {
+    const value = (await result)[name];
+    if (expected !== undefined) {
+      if (name === "status" && expected === "non-zero") {
+        expect(value).not.toBe(0);
+      } else if (typeof expected === "function") {
+        expected(value);
+      } else {
+        expect(value).toEqual(expected);
+      }
+    } else {
+      expect(value).toMatchSnapshot();
+    }
+  });
+}
+
+function runCli(dir, args = [], options = {}) {
+  const result = runCommand(dir, args, options);
+  return {
+    get status() {
+      return result.then(({ status }) => status);
+    },
+    get stdout() {
+      return run().then(({ stdout }) => stdout);
+    },
+    get stderr() {
+      return run().then(({ stderr }) => stderr);
+    },
+    get write() {
+      return run().then(({ write }) => write);
+    },
+    test: (tests) => {
+      for (const name of ["status", "stdout", "stderr", "write"]) {
+        const expected = tests[name];
+        runTest(name, expected, result, options);
+      }
+    },
+    then: (onFulfilled, onRejected) => {
+      return result.then(onFulfilled, onRejected);
+    },
+  };
+}
+
+export { runCli };
