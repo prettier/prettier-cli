10652d71b18d6bde17b449c516b271d51c89a1a6
Fabio Spampinato <spampinabio@gmail.com>
2024-09-14 21:00:28 +0100

    Ported test suite: plugin-precedence

---
+++ test/__tests__/plugin-precedence.js
@@ -0,0 +1,16 @@
+import { runCli } from "../utils";
+
+describe("json-stringify takes precedence over json for package.json", () => {
+  runCli("/", [
+    "--stdin-filepath=package.json",
+  ], {
+    input:
+      '{ "a": "longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong" }',
+  }).test({
+    stdout:
+      '{\n  "a": "longlonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglonglong"\n}',
+    stderr: "",
+    status: 0,
+    write: [],
+  });
+});
