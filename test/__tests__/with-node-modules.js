ae58b4aa5785c64662b8f0047139dee20ee2ed67
Fabio Spampinato <spampinabio@gmail.com>
2024-09-14 20:48:13 +0100

    Ported test suite: with-node-modules
    - Explicitly provided files inside node_modules are not ignored.

---
+++ test/__tests__/with-node-modules.js
@@ -0,0 +1,41 @@
+import { runCli } from "../utils";
+
+describe("ignores node_modules by default", () => {
+  runCli("with-node-modules", [
+    "**/*.js",
+    "-l",
+  ]).test({
+    status: 1,
+  });
+});
+
+describe("ignores node_modules by with ./**/*.js", () => {
+  runCli("with-node-modules", [
+    "./**/*.js",
+    "-l",
+  ]).test({
+    status: 1,
+  });
+});
+
+describe("doesn't ignore node_modules with --with-node-modules flag", () => {
+  runCli("with-node-modules", [
+    "**/*.js",
+    "-l",
+    "--with-node-modules",
+  ]).test({
+    status: 1,
+  });
+});
+
+describe("does not ignore node_modules for manual file list", () => {
+  runCli("with-node-modules", [
+    "node_modules/node-module.js",
+    "not_node_modules/file.js",
+    "nested/node_modules/node-module.js",
+    "regular-module.js",
+    "-l",
+  ]).test({
+    status: 1,
+  });
+});
