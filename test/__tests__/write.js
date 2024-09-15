13825b86d886b85283d032d0b866a126bdfc69c2
Fabio Spampinato <spampinabio@gmail.com>
2024-09-15 13:49:55 +0100

    Partially ported test suite: write

---
+++ test/__tests__/write.js
@@ -0,0 +1,41 @@
+import { runCli } from "../utils";
+
+//TODO: Align the output for the following tests
+
+describe.skip("write file with --write + unformatted file", () => {
+  runCli("write", [
+    "--write",
+    "unformatted.js",
+  ]).test({
+    status: 0,
+  });
+});
+
+describe.skip("write file with -w + unformatted file", () => {
+  runCli("write", [
+    "-w",
+    "unformatted.js",
+  ]).test({
+    status: 0,
+  });
+});
+
+describe.skip("do not write file with --write + formatted file", () => {
+  runCli("write", [
+    "--write",
+    "formatted.js",
+  ]).test({
+    write: [],
+    status: 0,
+  });
+});
+
+describe.skip("do not write file with --write + invalid file", () => {
+  runCli("write", [
+    "--write",
+    "invalid.js",
+  ]).test({
+    write: [],
+    status: "non-zero",
+  });
+});
