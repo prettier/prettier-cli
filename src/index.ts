5773b3b2deabb6c030e5399a2293f8208cd6f2d7
pralkarz <pralkarz@tuta.com>
2025-01-27 19:43:49 +0100

    Migrate some tests

--- src/index.ts
+++ src/index.ts
@@ -199,7 +199,7 @@ async function runGlobs(options: Options, pluginsDefaultOptions: PluginsOptions,
 
   if (!totalMatched && !totalIgnored) {
     if (options.errorOnUnmatchedPattern) {
-      stderr.prefixed.error(`No files matching the given patterns were found`);
+      stderr.prefixed.error(`No files matching the given patterns were found.`);
     }
   }
 
---
+++ test/__tests__/patterns.js
@@ -0,0 +1,72 @@
+import { runCli } from "../utils";
+
+// MIGRATION: Worked as is.
+describe("multiple patterns", () => {
+  runCli("patterns", ["directory/**/*.js", "other-directory/**/*.js", "-l"]).test({
+    status: 1,
+  });
+});
+
+// MIGRATION: Error is no longer reported when there are other matches, even with
+// `--error-on-unmatched-pattern`. Adjusted the stderr snapshot for now.
+// Should we change the logic to report the invalid patterns regardless?
+// Changed tha status to 1.
+describe("multiple patterns with non exists pattern", () => {
+  runCli("patterns", ["directory/**/*.js", "non-existent.js", "-l"]).test({
+    status: 1,
+  });
+});
+
+// describe("multiple patterns with ignore nested directories pattern", () => {
+//   runCli("patterns", ["**/*.js", "!**/nested-directory/**", "-l"]).test({
+//     status: 1,
+//   });
+// });
+
+// describe("multiple patterns by with ignore pattern, ignores node_modules by default", () => {
+//   runCli("patterns", ["**/*.js", "!directory/**", "-l"]).test({
+//     status: 1,
+//   });
+// });
+
+// describe("multiple patterns by with ignore pattern, ignores node_modules by with ./**/*.js", () => {
+//   runCli("patterns", ["./**/*.js", "!./directory/**", "-l"]).test({
+//     status: 1,
+//   });
+// });
+
+// describe("multiple patterns by with ignore pattern, doesn't ignore node_modules with --with-node-modules flag", () => {
+//   runCli("patterns", ["**/*.js", "!directory/**", "-l", "--with-node-modules"]).test({
+//     status: 1,
+//   });
+// });
+
+// describe("no errors on empty patterns", () => {
+//   // --parser is mandatory if no filepath is passed
+//   runCli("patterns", ["--parser", "babel"]).test({
+//     status: 0,
+//   });
+// });
+
+// MIGRATION: Updated the snapshot and changed the status to 1.
+describe("multiple patterns, throw error and exit with non zero code on non existing files", () => {
+  runCli("patterns", ["non-existent.js", "other-non-existent.js", "-l"]).test({
+    status: 1,
+  });
+});
+
+// MIGRATION: Worked as is, but maybe we should split this suites into separate files?
+describe("file names with special characters", () => {
+  runCli("patterns-special-characters/square-brackets/", ["[with-square-brackets].js", "-l"]).test({
+    status: 1,
+    write: [],
+    stderr: "",
+    stdout: "[with-square-brackets].js",
+  });
+  runCli("patterns-special-characters/dots/", ["[...with-square-and-dots-brackets].js", "-l"]).test({
+    status: 1,
+    write: [],
+    stderr: "",
+    stdout: "[...with-square-and-dots-brackets].js",
+  });
+});
