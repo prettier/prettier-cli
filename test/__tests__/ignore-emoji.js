933aa3d0dd65ce74456ed854a5835043d38072aa
James Garbutt <43081j@users.noreply.github.com>
2025-01-29 16:07:38 +0000

    test: add ignore-emoji tests

---
+++ test/__tests__/ignore-emoji.js
@@ -0,0 +1,16 @@
+import { runCli } from "../utils";
+
+describe("ignores file name contains emoji", () => {
+  runCli("ignore-emoji", ["**/*.js", "-l"]).test({
+    status: 1,
+  });
+});
+
+// TODO (43081j): re-enable once we fix #21
+describe.skip("stdin", () => {
+  runCli("ignore-emoji", ["--stdin-filepath", "ignored/我的样式.css"], {
+    input: ".name {                         display: none; }",
+  }).test({
+    status: 0,
+  });
+});
