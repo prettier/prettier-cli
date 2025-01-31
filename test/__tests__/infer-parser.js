fb5c6b15c07da9b31e440f95c28b956eef2af1a4
James Garbutt <43081j@users.noreply.github.com>
2025-01-31 16:44:19 +0000

    test: add infer-parser tests
    Adds the `infer-parser` tests from prettier.
    
    Also updates the `runCLI` util to only execute the binary once a test
    actually runs.

---
+++ test/__tests__/infer-parser.js
@@ -0,0 +1,156 @@
+import { runCli } from "../utils";
+
+describe("stdin no path and no parser", () => {
+  describe("logs error and exits with 2", () => {
+    runCli("infer-parser/", [], { input: "foo" }).test({
+      status: 1,
+      stdout: "",
+      write: [],
+    });
+  });
+
+  // TODO (43081j): in prettier, this tests that it exits with 0 for
+  // whatever reason. should we do the same?
+  describe("--check logs error but exits with 1", () => {
+    runCli("infer-parser/", ["--check"], {
+      input: "foo",
+    }).test({
+      status: 1,
+      stdout: "",
+      write: [],
+    });
+  });
+
+  // TODO (43081j): in prettier, this tests that it exits with 0 for
+  // whatever reason. should we do the same?
+  describe("--list-different logs error but exits with 1", () => {
+    runCli("infer-parser/", ["--list-different"], {
+      input: "foo",
+    }).test({
+      status: 1,
+      stdout: "",
+      write: [],
+    });
+  });
+});
+
+describe("stdin with unknown path and no parser", () => {
+  describe("logs error and exits with 2", () => {
+    runCli("infer-parser/", ["--stdin-filepath", "foo"], {
+      input: "foo",
+    }).test({
+      status: 1,
+      stdout: "",
+      write: [],
+    });
+  });
+
+  // TODO (43081j): in prettier, this tests that it exits with 0 for
+  // whatever reason. should we do the same?
+  describe("--check logs error but exits with 1", () => {
+    runCli("infer-parser/", ["--check", "--stdin-filepath", "foo"], {
+      input: "foo",
+    }).test({
+      status: 1,
+      stdout: "",
+      write: [],
+    });
+  });
+
+  // TODO (43081j): in prettier, this tests that it exits with 0 for
+  // whatever reason. should we do the same?
+  describe("--list-different logs error but exits with 1", () => {
+    runCli(
+      "infer-parser/",
+      ["--list-different", "--stdin-filepath", "foo"],
+      { input: "foo" },
+    ).test({
+      status: 1,
+      stdout: "",
+      write: [],
+    });
+  });
+});
+
+describe("unknown path and no parser", () => {
+  describe("specific file is ignored", () => {
+    runCli("infer-parser/", ["--end-of-line", "lf", "FOO"]).test({
+      status: 0,
+      stdout: "",
+      write: [],
+    });
+  });
+
+  // TODO (43081j): this currently eats parser errors up, we need to decide
+  // if when !check && !list && !write we should still log parser errors to
+  // stderr
+  describe("multiple files", () => {
+    runCli("infer-parser/", ["--end-of-line", "lf", "*"]).test({
+      status: 1,
+      write: [],
+    });
+  });
+});
+
+describe("--check with unknown path and no parser", () => {
+  describe("specific file is ignored", () => {
+    runCli("infer-parser/", ["--check", "FOO"]).test({
+      status: 0,
+      write: [],
+    });
+  });
+
+  describe("multiple files", () => {
+    runCli("infer-parser/", ["--check", "*"]).test({
+      status: 1,
+      write: [],
+    });
+  });
+});
+
+describe("--list-different with unknown path and no parser", () => {
+  describe("specific file should be ignored", () => {
+    runCli("infer-parser/", ["--list-different", "FOO"]).test({
+      status: 0,
+      stdout: "",
+      write: [],
+    });
+  });
+
+  describe("multiple files", () => {
+    runCli("infer-parser/", ["--list-different", "*"]).test({
+      status: 1,
+      stdout: "foo.js",
+      write: [],
+    });
+  });
+});
+
+describe("--write with unknown path and no parser", () => {
+  describe("specific file should be ignored", () => {
+    runCli("infer-parser/", ["--write", "FOO"]).test({
+      status: 0,
+      stdout: "",
+      write: [],
+    });
+  });
+
+  describe("multiple files", () => {
+    runCli("infer-parser/", ["--write", "*"]).test({
+      status: 1,
+    });
+  });
+});
+
+describe("Known/Unknown", () => {
+  runCli("infer-parser/known-unknown", [
+    "--end-of-line",
+    "lf",
+    "--list-different",
+    ".",
+  ]).test({
+    status: 1,
+    stderr: "",
+    write: [],
+  });
+});
