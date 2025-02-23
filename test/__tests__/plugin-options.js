6262bea153aaf2e7e4776419e83ef0344f6cbc65
James Garbutt <43081j@users.noreply.github.com>
2025-02-23 16:44:26 +0000

    Ported test suite: plugin-options
    - `--help option` is no longer supported at the moment.
    - Config resolution for files piped in through stdin is not wired in yet.

---
+++ test/__tests__/plugin-options.js
@@ -0,0 +1,44 @@
+import { runCli } from "../utils";
+
+test("show external options with `--help`", async () => {
+  const originalStdout = await runCli("plugin-options", ["--help"]).stdout;
+  const pluggedStdout = await runCli("plugin-options", ["--help", "--plugin=./plugin.cjs"]).stdout;
+
+  const originalLines = originalStdout.split("\n");
+  const pluggedLines = pluggedStdout.split("\n");
+  const differentLines = pluggedLines.filter((line) => !originalLines.includes(line));
+  expect(differentLines.join("\n")).toMatchSnapshot();
+});
+
+describe("external options from CLI should work", () => {
+  runCli("plugin-options", [
+    "--plugin=./plugin.cjs",
+    "--stdin-filepath",
+    "example.foo",
+    "--foo-option",
+    "baz",
+  ], {
+    input: "hello-world",
+  }).test({
+    stdout: "foo:baz",
+    stderr: "",
+    status: 0,
+    write: [],
+  });
+});
+
+// TODO (43081j): re-enable this once #21 is fixed
+describe.skip("external options from config file should work", () => {
+  runCli("plugin-options", [
+    "--config-path=./config.json",
+    "--stdin-filepath",
+    "example.foo",
+  ], {
+    input: "hello-world",
+  }).test({
+    stdout: "foo:baz",
+    stderr: "",
+    status: 0,
+    write: [],
+  });
+});
