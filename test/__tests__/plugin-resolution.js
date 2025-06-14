08c7aef317f9a15a080979e89817b75a123eeabb
James Garbutt <43081j@users.noreply.github.com>
2025-06-14 21:01:26 +0200

    Ported test suite: plugin-resolution (#42)
    - We currently don't support loading plugins with implicit relative paths (missing starting dot)

---
+++ test/__tests__/plugin-resolution.js
@@ -0,0 +1,84 @@
+import { runCli } from "../utils";
+
+describe("loads --plugin by its relative path", () => {
+  runCli("plugins", [
+    "automatic/file.txt",
+    "--parser=bar",
+    "--plugin=./automatic/node_modules/prettier-plugin-bar/index.js",
+  ]).test({
+    stdout: "content from `prettier-plugin-bar` package + contents",
+    stderr: "",
+    status: 0,
+    write: [],
+  });
+});
+
+// TODO (43081j): we don't currently support loading relative paths without
+// the leading `./`
+describe.skip("loads --plugin by its relative path without leading ./", () => {
+  runCli("plugins", [
+    "automatic/file.txt",
+    "--parser=bar",
+    "--plugin=automatic/node_modules/prettier-plugin-bar/index.js",
+  ]).test({
+    stdout: "content from `prettier-plugin-bar` package + contents",
+    stderr: "",
+    status: 0,
+    write: [],
+  });
+});
+
+describe("loads --plugin by package name", () => {
+  runCli("plugins/automatic", [
+    "file.txt",
+    "--parser=foobar",
+    "--plugin=@user/prettier-plugin-foobar",
+  ]).test({
+    stdout: "foobar+contents",
+    stderr: "",
+    status: 0,
+    write: [],
+  });
+});
+
+// TODO (43081j): we don't currently support loading relative paths without
+// the leading `./`
+describe.skip("loads --plugin by filename without leading ./, should resolve to file, not package", () => {
+  runCli("plugins/automatic", [
+    "file.txt",
+    "--parser=baz",
+    "--plugin=prettier-plugin-baz.js",
+  ]).test({
+    stdout: "content from `prettier-plugin-baz.js` file + contents",
+    stderr: "",
+    status: 0,
+    write: [],
+  });
+});
+
+describe("loads --plugin by bespoke plugin name (assuming it is installed in cwd)", () => {
+  runCli("plugins/bespoke", [
+    "../automatic/file.txt",
+    "--parser=bespoke",
+    "--plugin=@company/prettier-plugin-bespoke/main.js",
+  ]).test({
+    stdout: "bespoke+contents",
+    stderr: "",
+    status: 0,
+    write: [],
+  });
+});
+
+// TODO (43081j): re-enable this once #21 is fixed
+describe.skip("support absolute path in config file", () => {
+  runCli("plugins/absolute-path", [
+    "--parser=uppercase-rocks"
+  ], {
+    input: "prettier should be uppercase",
+  }).test({
+    stdout: "PRETTIER SHOULD BE UPPERCASE",
+    stderr: "",
+    status: 0,
+    write: [],
+  });
+});
